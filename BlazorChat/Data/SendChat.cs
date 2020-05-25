22f3cec7482a9a87ccf06ad2b67addfa4e52f89d
Rockford Lhotka <rocky@lhotka.net>
2020-05-24 23:25:06 -0500

    Initial commit

---
+++ BlazorChat/Data/SendChat.cs
@@ -0,0 +1,43 @@
+﻿using Csla;
+using System;
+
+namespace BlazorChat.Data
+{
+  [Serializable]
+  public class SendChat : CommandBase<SendChat>
+  {
+    public static readonly PropertyInfo<string> NameProperty = RegisterProperty<string>(nameof(Name));
+    public string Name
+    {
+      get => ReadProperty(NameProperty);
+      set => LoadProperty(NameProperty, value);
+    }
+
+    public static readonly PropertyInfo<string> TextProperty = RegisterProperty<string>(nameof(Text));
+    public string Text
+    {
+      get => ReadProperty(TextProperty);
+      set => LoadProperty(TextProperty, value);
+    }
+
+    [Create]
+    [RunLocal]
+    private void Create(User user, string text)
+    {
+      Name = user.Name;
+      Text = text;
+    }
+
+    [Execute]
+    private void Execute()
+    {
+      var text = $"{Name}: {Text}";
+      lock (App.Messages)
+      {
+        App.Messages.Add(text);
+        if (App.Messages.Count > 20)
+          App.Messages.RemoveAt(0);
+      }
+    }
+  }
+}
---
+++ BlazorChat/Data/User.cs
@@ -0,0 +1,34 @@
+﻿using Csla;
+using System;
+
+namespace BlazorChat.Data
+{
+  [Serializable]
+  public class User : BusinessBase<User>
+  {
+    public static readonly PropertyInfo<string> IdProperty = RegisterProperty<string>(nameof(Id));
+    public string Id
+    {
+      get => GetProperty(IdProperty);
+      private set => LoadProperty(IdProperty, value);
+    }
+
+    public static readonly PropertyInfo<string> NameProperty = RegisterProperty<string>(nameof(Name));
+    public string Name
+    {
+      get => GetProperty(NameProperty);
+      set => SetProperty(NameProperty, value);
+    }
+
+    [Create]
+    [RunLocal]
+    private void Create(string name)
+    {
+      using (BypassPropertyChecks)
+      {
+        Id = Guid.NewGuid().ToString();
+        Name = name;
+      }
+    }
+  }
+}
